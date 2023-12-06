bfcadfdffe0d1d2a57f4b515a37bc2ea156db94d
Faradar <tomi_compuamste@hotmail.com>
2023-12-06 00:32:46 -0300

    feature: added responsive low-fidelity navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import { Quicksand } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// Components
+import Navbar from "../components/navbar/Navbar";
+
+const quicksand = Quicksand({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={quicksand.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
