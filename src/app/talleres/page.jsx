0613f5a4eb69bff42b69b3cae9847fcd18a0824e
MaicolMarcenaro <maicol.marcenaro@gmail.com>
2023-12-17 22:22:39 -0300

    Cambios talleres

--- src/app/talleres/page.jsx
+++ src/app/talleres/page.jsx
@@ -4,45 +4,51 @@ import './talleres.css'
 
 export default function Talleres() {
     return (
-      <div>
+      <div className="m-5">
         <div className="con_magin">
             <p className="text_talleres">
-                Talleres de arte para niños y adultos, utilizando materiales cotidianos y promoviendo.
+            Talleresde arte para niños y adultos, utilizando materiales cotidianos y promoviendo.
             </p>
             <div className="modulos">
-                <div className="modulo1">
-                    <Link
+                <Link
                     href="/ninos"
                     className="modulo"
                     >
-                        NINOS
-                    </Link>
-                </div>
-                <div className="modulo2">
-                    <Link
-                    href="/adultos"
-                    className="modulo"
-                    >
-                        ADULTOS
-                    </Link>
-                </div>
+                    <div className="modulo1"></div>
+                </Link>
+                <Link
+                href="/adultos"
+                className="modulo"
+                >
+                    <div className="modulo2"></div>
+                </Link>
             </div>
             <div className="descrption_talleres">
-                <div className="descrption_taller">
-                    Descripción del taller
+                    <div className="descrption_taller">
+                        <h2 className="text-center mb-5 font-bold ">Descripción del taller</h2>
+                        <p className="lg:text-base">
+                        Creamos este espacio para fomentar la creatividad y el juego en los niños, enseñándoles a hacer arte con materiales que se encuentran en cualquier casa, generando conciencia de reutilizar , reciclar y resignificar materiales a través de distintas técnicas.
+                        </p>
+                    </div>
+                    <div className="descrption_taller">
+                        <h2 className="text-center mb-5 font-bold ">¿Qué aprenderán?</h2>
+                        <p className="lg:text-base">
+                            A encontrar espacios de sociabilización con sus pares, mediante actividades lúdicas ligadas a las artes plásticas , el aprendizaje de técnicas y el conocimiento de conceptos y curiosidades de distintos artistas. 
+                        </p>
+                    </div>
                 </div>
-                <div className="descrption_taller">
-                    ¿Qué aprenderán?
-                </div>
-            </div>
         </div>
         <div>
-            <p className="m-10">Galería de imágenes de los talleres</p>
-            <div className="p-5 con_talleres_img">
-                <div className="talleres_img"></div>
-                <div className="talleres_img"></div>
-                <div className="talleres_img"></div>
-                <div className="talleres_img"></div>
+            <p className="font-bold  texttalleres">Galería de imágenes de los talleres</p>
+            <div className="p-5 con_talleres_img place-items-center">
+                <div className="talleres_img bg-cover bg-center h-screen" style={{ backgroundImage: "url('/img/IMG_20230818_172900 3.png')" }}></div>
+                <div className="talleres_img bg-cover bg-center h-screen " style={{ backgroundImage: "url('/img/IMG_20230804_164714 2.png')" }}></div>
+                <div className="talleres_img bg-cover bg-center h-screen " style={{ backgroundImage: "url('/img/IMG_20230811_172914 3.png')" }}></div>
+                <div className="talleres_img bg-cover bg-center h-screen " style={{ backgroundImage: "url('/img/IMG_20230811_165614 3.png')" }}></div>
+                <div className="talleres_img bg-cover bg-center h-screen " style={{ backgroundImage: "url('/img/IMG_20230825_170557 2.png')" }}></div>
+                <div className="talleres_img bg-cover bg-center h-screen " style={{ backgroundImage: "url('/img/IMG_20230804_164725 2.png')" }}></div>
+                <div className="talleres_img bg-cover bg-center h-screen " style={{ backgroundImage: "url('/img/IMG_20230825_170547 1.png')" }}></div>
+                <div className="talleres_img bg-cover bg-center h-screen " style={{ backgroundImage: "url('/img/IMG_20230811_165351 1.png')" }}></div>
             </div>
         </div>
       </div>
