f063da6532857b7e78eee72a2640f5a809f966c2
Faradar <tomi_compuamste@hotmail.com>
2024-01-28 21:07:06 -0300

    feature: high fidelity home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 "use client";
-import React from "react";
-// import Link from "next/link";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import Carousel from "@/components/carousel/Carousel";
 import logo from "../../public/contacto/logo.svg";
-import HomeCard from "@/components/home/HomeCard";
+import HomeCardList from "@/components/home/HomeCardList";
 import taza from "../../public/inicio/tazaDienteDeLeon.jpg";
 import llavero from "../../public/inicio/llaveroLechuza.jpg";
 import calcomanias from "../../public/inicio/Calcomanias.jpg";
@@ -18,68 +16,126 @@ import miceMarron from "../../public/inicio/miceMarronHome.svg";
 import marronBorde from "../../public/inicio/marronBordeHome.svg";
 
 export default function Home() {
+  const [isMd, setIsMd] = useState(false);
+
+  useEffect(() => {
+    const mdMediaQuery = window.matchMedia("(min-width: 768px)");
+    setIsMd(mdMediaQuery.matches);
+
+    const handleResize = () => {
+      setIsMd(mdMediaQuery.matches);
+    };
+
+    mdMediaQuery.addEventListener("change", handleResize);
+
+    return () => {
+      mdMediaQuery.removeEventListener("change", handleResize);
+    };
+  }, []);
+
+  const miscRightColor = isMd
+    ? "/inicio/misc-color-right.svg"
+    : "/inicio/misc-color-right-mobile.svg";
+
+  const miscRightOutline = isMd
+    ? "/inicio/misc-outline-right.svg"
+    : "/inicio/misc-outline-right-mobile.svg";
+
   const carouselImages = [
-    "/inicio/1.svg",
-    "/inicio/2.svg",
-    "/inicio/3.svg",
-    "/inicio/4.svg",
-    "/inicio/5.svg",
-    "/inicio/6.svg",
+    "/inicio/carousel3.jpg",
+    "/inicio/carousel4.jpg",
+    "/inicio/carousel5.jpg",
+    "/inicio/carousel1.jpg",
+    "/inicio/carousel2.jpg",
+  ];
+
+  const productos = [
+    { src: llavero, title: "Llavero rectangular Lechuza vizcachera" },
+    { src: taza, title: "Taza cerámica, diseño con Dientes de León" },
+    { src: calcomanias, title: "Calcamonías Flora autóctona" },
+    { src: botella, title: "Botella con diseño de Mariposa" },
+  ];
+
+  const talleres = [
+    {
+      src: talleres1,
+      title:
+        "Enseñar que se puede hacer arte con lo cotidiano, con lo que tenemos a nuestro alrededor.",
+    },
+    {
+      src: talleres2,
+      title:
+        "Espacio para conectarte con tu interior y donde se tenga la posibilidad de expresar las emociones a través del arte.",
+    },
+    {
+      src: talleres3,
+      title:
+        "Introducir el concepto de reutilización y reciclado a través del ARTE.",
+    },
+    {
+      src: talleres4,
+      title:
+        "Talleres de arte para niños y adultos, utilizando materiales cotidianos y promoviendo.",
+    },
   ];
 
   return (
     <main>
-      <section className="grid grid-cols-12 justify-center bg-slate-700/10 shadow-[0_6px_6px_-1px_rgba(0,0,0,0.3)] overflow-y-hidden">
-        <div className="w-32 md:w-44 lg:w-48 grid z-10">
+      <section className="grid grid-cols-12 justify-center bg-slate-700/10 shadow-[0_6px_6px_-1px_rgba(0,0,0,0.3)] overflow-hidden relative">
+        <div className="w-32 md:w-44 lg:w-64 2xl:w-72 grid z-10 2xl:translate-y-[-10px]">
           <Image
-            src="/inicio/misc-outline-left-mobile.svg"
+            src="/inicio/misc-outline-left.svg"
             width={80}
             height={200}
             alt="borde micelania amarrila"
+            className="md:w-40 lg:w-52 2xl:w-[260px] 2xl:translate-y-[-10px]"
           />
-          <div className="grid absolute w-28 md:w-40 md:top-28 lg:w-48">
+          <div className="grid absolute">
             <Image
-              src="/inicio/misc-color-left-mobile.svg"
+              src="/inicio/misc-color-left.svg"
               width={70}
               height={200}
               alt="micelania amarilla"
+              className="md:w-36 lg:w-48 2xl:w-[250px] 2xl:translate-x-[-10px]"
             />
           </div>
         </div>
         <div className="flex flex-col text-center w-full col-span-10 relative mx-0 px-0">
-          <h1 className="font-semibold md:text-3xl">Bienvenidos!</h1>
+          <h1 className="font-semibold md:text-[32px] md:mt-6">Bienvenidos!</h1>
           <Image
             src={logo}
             width={182}
             height={200}
-            className="mx-auto md:w-11/12 lg:w-12/12"
+            className="mx-auto max-w-[920px] md:w-11/12 md:mt-[-20px] lg:w-12/12"
             alt="logo de pa'l alma grande"
           />
-          <p className="text-xs md:text-base font-semibold py-2 w-64 mx-auto">
+          <p className="text-xs md:text-xl font-semibold py-2 w-64 max-w-[920px] md:w-auto mx-auto">
             “En el corazón de “Pa'l alma” late el amor por la diversidad
             biológica de Argentina. Nuestros productos son una manifestación de
             esta pasión, capturando la esencia única de la flora y fauna que nos
             rodea.”
           </p>
         </div>
-        <div className="w-28 md:w-28 lg:w-40 grid justify-end absolute top-32 lg:top-24 md:top-28 right-[-30px]">
+        <div className="w-28 md:w-48 lg:w-64 2xl:w-96 grid justify-items-end absolute top-[-20px] md:top-[-50px] lg:top-[-70px] 2xl:top-[-110px] right-[-1px]">
           <Image
-            src="/inicio/misc-color-right-mobile.svg"
-            width={80}
+            src={miscRightColor}
+            width={70}
             height={200}
             alt="borde de micelania azul 3"
+            className="md:w-[300px] lg:w-[400px] 2xl:w-[500px]"
           />
-          <div className="w-24 md:w-32 lg:w-36 grid relative bottom-[190px] md:bottom-80 md:right-14 lg:bottom-96 lg:right-16 right-0">
+          <div className="w-24 md:w-48 lg:w-64 2xl:w-96 grid justify-items-end relative bottom-[200px] md:bottom-[280px] lg:bottom-[370px] 2xl:bottom-[560px] right-0">
             <Image
-              src="/inicio/misc-outline-right-mobile.svg"
-              width={80}
+              src={miscRightOutline}
+              width={65}
               height={200}
               alt="micelania azul"
+              className="md:w-[300px] 2xl:w-[500px]"
             />
           </div>
         </div>
       </section>
-      <div className="flex items-center justify-center overflow-hidden">
+      <div className="flex items-center justify-center overflow-hidden mb-10">
         <Carousel images={carouselImages} />
       </div>
       <div className="hidden lg:flex absolute right-0 top-full z-[-10]">
@@ -96,62 +152,13 @@ export default function Home() {
           alt="micelania marron"
         />
       </div>
-      <section className="w-11/12 mx-auto mb-10">
+      <section className="w-11/12 m-auto 2xl:mr-20 mb-10">
         <h2 className="font-bold text-left pl-8 text-xl lg:text-3xl">
           ALGUNOS DE NUESTROS PRODUCTOS:
         </h2>
-        <div className="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-          <Link href="/productos">
-            <HomeCard
-              src={llavero}
-              title={"Llavero rectangular Lechuza vizcachera"}
-            />
-          </Link>
-          <Link href="/productos">
-            <HomeCard
-              src={taza}
-              title={"Taza cerámica, diseño con Dientes de León"}
-            />
-          </Link>
-          <Link href="/productos">
-            <HomeCard src={calcomanias} title={"Calcamonías Flora autóctona"} />
-          </Link>
-          <Link href="/productos">
-            <HomeCard src={botella} title={"Botella con diseño de Mariposa"} />
-          </Link>
-
-          <Link href="/talleres">
-            <HomeCard
-              src={talleres1}
-              title={
-                "Enseñar que se puede hacer arte con lo cotidiano, con lo que tenemos a nuestro alrededor."
-              }
-            />
-          </Link>
-          <Link href="/talleres">
-            <HomeCard
-              src={talleres2}
-              title={
-                "Espacio para conectarte con tu interior y donde se tenga la posibilidad de expresar las emociones a través del arte."
-              }
-            />
-          </Link>
-          <Link href="/talleres">
-            <HomeCard
-              src={talleres3}
-              title={
-                "Introducir el concepto de reutilización y reciclado a través del ARTE."
-              }
-            />
-          </Link>
-          <Link href="/talleres">
-            <HomeCard
-              src={talleres4}
-              title={
-                "Talleres de arte para niños y adultos, utilizando materiales cotidianos y promoviendo."
-              }
-            />
-          </Link>
+        <div className="mx-auto grid 2xl:gap-x-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+          <HomeCardList data={productos} linkHref="/productos" />
+          <HomeCardList data={talleres} linkHref="/talleres" />
         </div>
       </section>
     </main>
